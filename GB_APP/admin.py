8f42f8b72432ae1f74bb0f0cf8174548c6d4bff3
JangYoHan <dygks5412@naver.com>
2022-05-14 15:52:27 +0900

    Sign-in&up

--- GB_APP/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from GB_APP.models import Test
-
-admin.site.register(Test)
--- GB_APP/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from GB_APP.models import Test
-
-class TestSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Test
-        fields = ('test', 'id')
